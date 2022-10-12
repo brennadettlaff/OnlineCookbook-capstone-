2bbc8ef444a40e5c1a0fd45df1003796b2989274
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-12 14:52:28 -0600

    created and registered apps

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,7 +40,12 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'cars.apps.CarsConfig'  # This app is for example use only
+    'cars.apps.CarsConfig',  # This app is for example use only
+    'collection',
+    'ingredients',
+    'notes',
+    'recipe',
+    'recipe-ingredients',
 ]
 
 MIDDLEWARE = [
---
+++ backend/recipe_ingredients/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RecipeIngredientsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'recipe_ingredients'
