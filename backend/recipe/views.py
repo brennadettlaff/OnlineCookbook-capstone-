85279ddf58ff6a46dd4b4034e15fa7f43ef375bd
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-13 13:33:51 -0600

    created views function to get all recipes

--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,8 +1,15 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+
+from .models import Recipe
+from .serializers import RecipeSerializer
 
 
 @api_view(['GET'])
-def recipe_list(request):
-
-    return Response('ok')
+@permission_classes([AllowAny])
+def recipe_list(request):  
+    if request.method == 'GET':
+        recipe = Recipe.objects.all()
+        serializer = RecipeSerializer(recipe, many=True)
+        return Response(serializer.data)
