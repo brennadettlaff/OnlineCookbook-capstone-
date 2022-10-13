a87301b30c23a14449f0d0d5ee1bc96a27d4fff5
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-13 13:59:09 -0600

    created post request in recipe app to create new entry

--- backend/recipe/urls.py
+++ backend/recipe/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list),
+    path('create/', views.create_recipe),
 ]
--- backend/recipe/views.py
+++ backend/recipe/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
@@ -13,3 +15,13 @@ def recipe_list(request):
         recipe = Recipe.objects.all()
         serializer = RecipeSerializer(recipe, many=True)
         return Response(serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def create_recipe(request):
+    if request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user = request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
