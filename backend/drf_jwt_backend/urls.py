93648a98f2d851d69569bfae3dbb8bcfa6ca7a89
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-13 19:51:34 -0600

    created url path for notes in settings + added get functionaliy

--- backend/drf_jwt_backend/urls.py
+++ backend/drf_jwt_backend/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('api/auth/', include('authentication.urls')),
     path('api/cars/', include('cars.urls')),
     path('api/recipe/', include('recipe.urls')),
-    path('api/ingredients/', include('ingredients.urls'))
+    path('api/ingredients/', include('ingredients.urls')),
+    path('api/notes/', include('notes.urls'))
 ]
--- backend/notes/views.py
+++ backend/notes/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Notes
+from .serializers import NotesSerializer
+
+
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def notes_list(request):  
+    if request.method == 'GET':
+        recipe = Notes.objects.all()
+        serializer = NotesSerializer(recipe, many=True)
+        return Response(serializer.data)
