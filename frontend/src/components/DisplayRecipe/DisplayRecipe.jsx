5396bd0496a9b7f97514cc45731409c14e52e1fc
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-16 17:30:43 -0600

    working on displaying recipe information

--- frontend/src/components/DisplayRecipe/DisplayRecipe.jsx
+++ frontend/src/components/DisplayRecipe/DisplayRecipe.jsx
@@ -20,42 +20,25 @@ const DisplayRecipe = () => {
     return ( 
         <div>
           <h1>test</h1>
+          <div>
+            {recipe.map((entry, index) => {
+              return(
+                <div key={index}>
+                  <h3>{entry.name}</h3>
+          </div>)})}
+        </div>
+          {/* <div>
+            {recipe &&
+              recipe.map((recipe) => (
+                <p key={recipe.id}>
+                  {recipe.name}
+                </p>
+              ))}
+          </div> */}
+          {/* <h1>{recipe[0].name}</h1> */}
         </div>
      );
 }
  
 export default DisplayRecipe;
 
-
-// const Disp = () => {
-//   const [cars, setCars] = useState([]);
-
-//   useEffect(() => {
-//     const getRecipe = async () => {
-//       try {
-//         let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
-//           headers: {
-//             Authorization: "Bearer " + token,
-//           },
-//         });
-//         setCars(response.data);
-//       } catch (error) {
-//         console.log(error.response.data);
-//       }
-//     };
-//     fetchCars();
-//   }, [token]);
-//   return (
-//     <div className="container">
-//       <h1>Home Page for {user.username}!</h1>
-//       {cars &&
-//         cars.map((car) => (
-//           <p key={car.id}>
-//             {car.year} {car.model} {car.make}
-//           </p>
-//         ))}
-//     </div>
-//   );
-// };
-
-// export default HomePage;
