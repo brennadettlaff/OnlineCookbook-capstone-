d991c850a496970dee160c78c81672e1ef96936b
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-15 20:33:48 -0600

    created display recipe component and created component function

---
+++ frontend/src/components/DisplayRecipe/DisplayRecipe.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import axios from "axios";
+
+
+const DisplayRecipe = () => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default DisplayRecipe;
+
