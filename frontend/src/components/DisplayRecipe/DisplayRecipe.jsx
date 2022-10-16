e7ed876a6b2f1c7b3febb630fe877612b41157ac
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-16 12:17:54 -0600

    added get recipe by id functionality

--- frontend/src/components/DisplayRecipe/DisplayRecipe.jsx
+++ frontend/src/components/DisplayRecipe/DisplayRecipe.jsx
@@ -5,12 +5,57 @@ import axios from "axios";
 
 
 const DisplayRecipe = () => {
+  const [recipe, setRecipe] = useState({});
+
+  useEffect(() => {
+    getRecipe();
+  }, [])
+
+  async function getRecipe(){
+    let response = await axios.get('http://127.0.0.1:8000/api/recipe/details/2/')
+    console.log(response.data)
+    setRecipe(response.data)
+  }
+
     return ( 
         <div>
-
+          <h1>test</h1>
         </div>
      );
 }
  
 export default DisplayRecipe;
 
+
+// const Disp = () => {
+//   const [cars, setCars] = useState([]);
+
+//   useEffect(() => {
+//     const getRecipe = async () => {
+//       try {
+//         let response = await axios.get("http://127.0.0.1:8000/api/cars/", {
+//           headers: {
+//             Authorization: "Bearer " + token,
+//           },
+//         });
+//         setCars(response.data);
+//       } catch (error) {
+//         console.log(error.response.data);
+//       }
+//     };
+//     fetchCars();
+//   }, [token]);
+//   return (
+//     <div className="container">
+//       <h1>Home Page for {user.username}!</h1>
+//       {cars &&
+//         cars.map((car) => (
+//           <p key={car.id}>
+//             {car.year} {car.model} {car.make}
+//           </p>
+//         ))}
+//     </div>
+//   );
+// };
+
+// export default HomePage;
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,12 +1,16 @@
 import React from "react";
 import useAuth from "../../hooks/useAuth";
+import DisplayRecipe from "../../components/DisplayRecipe/DisplayRecipe";
 
 
 const HomePage = () => {
   const [user, token] = useAuth();
 
   return (
+    <div>
       <div>Welcome {user.first_name}</div>
+      <DisplayRecipe />
+    </div>
   );
 };
 
