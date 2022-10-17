854833287b08a48b33c220badaf07502c9dfc64c
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-17 15:19:29 -0600

    added useParams to notes api call

--- frontend/src/components/Notes/Notes.jsx
+++ frontend/src/components/Notes/Notes.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { useParams } from 'react-router-dom';
 
 const Notes = () => {
 
     const [notes, setNote] = useState([]);
 
+    console.log("Params", useParams().id)
+    let recipeId = useParams().id
   
     useEffect(() => {
       getRecipe();
     }, [])
   
     async function getRecipe(){
-      let response = await axios.get(`http://127.0.0.1:8000/api/notes/view/1/`)
+      let response = await axios.get(`http://127.0.0.1:8000/api/notes/view/${recipeId}/`)
       console.log("recipe", response.data)
       setNote(response.data)
     }
