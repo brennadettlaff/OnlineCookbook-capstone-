b62276f5ea8715f141b0717834d68ac3970bdbe3
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-17 16:44:58 -0600

    created table for adding recipe

--- frontend/src/components/AddRecipe/AddRecipe.jsx
+++ frontend/src/components/AddRecipe/AddRecipe.jsx
@@ -4,7 +4,17 @@ const AddRecipe = () => {
     return ( 
         <div>
             <h1>test</h1>
+            <form >
+                <label>Name:</label>
+                <input type='text'></input>
+                <label>Description:</label>
+                <input type='text'></input>
+                <label>Time:</label>
+                <input type='text'></input>
+                <label>Instructions:</label>
+                <input type='text'></input>
 
+            </form>
         </div>
      );
 }
