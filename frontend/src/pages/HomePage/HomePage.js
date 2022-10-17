76b92b34d6083c16b13de47c0c83282c0abe79b8
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-17 13:00:39 -0600

    added functionality to get all recipes for single user

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import useAuth from "../../hooks/useAuth";
+import axios from "axios";
 
 
 const HomePage = () => {
-  const [user] = useAuth();
+  const [user, token] = useAuth();
+  const [recipe, setRecipe] = useState([])
+
+  useEffect(()=> {
+    getUsersRecipes();
+  }, [])
+
+  const getUsersRecipes = async () => {
+    let response = await axios.get('http://127.0.0.1:8000/api/recipe/user/', {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    })
+    console.log(response.data)
+    setRecipe(response.log)
+  }
 
   return (
     <div>
