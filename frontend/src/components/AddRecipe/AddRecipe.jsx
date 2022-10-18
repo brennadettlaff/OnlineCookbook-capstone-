590584a8c18358b69742e0c80e8aa876b76d450b
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-18 07:53:24 -0600

    created handle submit functionality for new recipe

--- frontend/src/components/AddRecipe/AddRecipe.jsx
+++ frontend/src/components/AddRecipe/AddRecipe.jsx
@@ -1,19 +1,36 @@
 import React, {useState} from "react";
 
 const AddRecipe = () => {
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [time, setTime] = useState('');
+    const [instructions, setInstructions] = useState('');
+
+    function handleSubmit(event){
+        event.preventDefault();
+        let newRecipe = {
+            name: name,
+            description: description,
+            time: time,
+            instructions: instructions,
+        };
+        console.log(name, description, time, instructions)
+    }
+
+
     return ( 
         <div>
             <h1>test</h1>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <label>Name:</label>
-                <input type='text'></input>
+                <input type='text' value={name} onChange={(event) => setName(event.target.value)}></input>
                 <label>Description:</label>
-                <input type='text'></input>
+                <input type='text' value={description} onChange={(event) => setDescription(event.target.value)}></input>
                 <label>Time:</label>
-                <input type='text'></input>
+                <input type='text' value={time} onChange={(event) => setTime(event.target.value)}></input>
                 <label>Instructions:</label>
-                <input type='text'></input>
-
+                <input type='text' value={instructions} onChange={(event) => setInstructions(event.target.value)}></input>
+                <button type='submit'>Add Recipe</button>
             </form>
         </div>
      );
