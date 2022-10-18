e3d9d39606b1b4a7a6bd8118f80c01afa1584626
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-18 14:47:57 -0600

    working on functionality to connect recipes and ingredients in linkrecipes page

--- frontend/src/components/LinkRecipes/LinkRecipes.jsx
+++ frontend/src/components/LinkRecipes/LinkRecipes.jsx
@@ -1,4 +1,27 @@
+import React, { useEffect } from "react";
+import useAuth from "../../hooks/useAuth";
+
+
 const LinkRecipes = (props) => {
+
+    const newJunction = {
+        id: id,
+        recipe_id: recipe_id,
+        ingredients_id: ingredients_id
+    }
+
+    async function addJunction(newJunction){
+        let response = await axios.post('http://127.0.0.1:8000/api/recipe_ingredients/add/', newJunction, {
+          headers: {
+            Authorization: "Bearer " + token,
+        },
+        })
+      }
+
+      useEffect(() => {
+        addJunction(newJunction)
+      })
+
     return ( 
         <div>
 
