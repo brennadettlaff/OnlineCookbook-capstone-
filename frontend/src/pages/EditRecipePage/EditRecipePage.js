103cf96e118dcc61d66e86f13f6951e35e3f0baf
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-19 14:24:17 -0600

    created buttons and navigations to take from home to new recipe page and from edit recipe to recipe display page

--- frontend/src/pages/EditRecipePage/EditRecipePage.js
+++ frontend/src/pages/EditRecipePage/EditRecipePage.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import useAuth from "../../hooks/useAuth";
 import axios from 'axios';
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import IngredientsForm from "../../components/IngredientsForm/IngredientsForm";
 import Ingredients from "../../components/Ingredients/Ingredients";
@@ -10,7 +11,9 @@ import Ingredients from "../../components/Ingredients/Ingredients";
 const EditRecipePage = () => {
     const [user, token] = useAuth();
     const [newIngredient, setNewIngredient] = useState([])
+
     let recipeId = useParams().id
+    let navigate = useNavigate()
 
     async function addNewIngredient(newIngredient){
         let response = await axios.post('http://127.0.0.1:8000/api/ingredients/add/', newIngredient, {
@@ -26,6 +29,7 @@ const EditRecipePage = () => {
             <Ingredients id={recipeId}/>
             <IngredientsForm addNewIngredient={addNewIngredient}/>
             {newIngredient.name}
+            <button onClick={() => navigate(`/recipe/${recipeId}`)}>DONE</button>
         </div>
      );
 }
--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,14 +1,17 @@
 import useAuth from "../../hooks/useAuth";
 import UserRecipes from "../../components/UserRecipes/UserRecipes";
+import { useNavigate } from "react-router-dom";
 
 
 const HomePage = () => {
   const [user] = useAuth();
 
+  let navigate = useNavigate();
 
   return (
     <div>
       <h1>Welcome {user.first_name}</h1>
+      <button onClick={() => navigate(`/addrecipe/`)}>Add Recipe</button>
       <h2>Recipes:</h2>
       <UserRecipes />
 
