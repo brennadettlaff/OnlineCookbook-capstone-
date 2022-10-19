17ce7fb6bf9a9d3d077b908704f25233c960c091
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-19 14:08:41 -0600

    created navigation in add recipe page to take to edit recipe page

--- frontend/src/pages/AddRecipePage/AddRecipePage.js
+++ frontend/src/pages/AddRecipePage/AddRecipePage.js
@@ -2,12 +2,15 @@ import RecipeForm from "../../components/RecipeForm/RecipeForm";
 import useAuth from "../../hooks/useAuth";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 
 
 const AddRecipePage = () => {
   const [user, token] = useAuth();
   const [newRecipe, setNewRecipe] = useState([])
+
+  let navigate = useNavigate();
 
   async function addNewRecipe(newRecipe){
     let response = await axios.post('http://127.0.0.1:8000/api/recipe/user/', newRecipe, {
@@ -16,6 +19,10 @@ const AddRecipePage = () => {
     },
     })
     setNewRecipe(response.data)
+    if(response.status === 201){
+      navigate(`/edit/${response.data.id}`);
+  }
+    
   }
 
   
