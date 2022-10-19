416dfe994b0c6fb763ec83f45197fa810223e468
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-19 10:50:20 -0600

    moved add new ingredient functionality from add recipe page to edit recipe page

--- frontend/src/pages/AddRecipePage/AddRecipePage.js
+++ frontend/src/pages/AddRecipePage/AddRecipePage.js
@@ -2,17 +2,12 @@ import RecipeForm from "../../components/RecipeForm/RecipeForm";
 import useAuth from "../../hooks/useAuth";
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import IngredientsForm from "../../components/IngredientsForm/IngredientsForm";
 
 
 
 const AddRecipePage = () => {
   const [user, token] = useAuth();
   const [newRecipe, setNewRecipe] = useState([])
-  const [newIngredient, setNewIngredient] = useState([])
-
-  console.log(newRecipe)
-  console.log(newIngredient)
 
   async function addNewRecipe(newRecipe){
     let response = await axios.post('http://127.0.0.1:8000/api/recipe/user/', newRecipe, {
@@ -23,21 +18,10 @@ const AddRecipePage = () => {
     setNewRecipe(response.data)
   }
 
-  async function addNewIngredient(newIngredient){
-    let response = await axios.post('http://127.0.0.1:8000/api/ingredients/add/', newIngredient, {
-      headers: {
-        Authorization: "Bearer " + token,
-    },
-    })
-    setNewIngredient(response.data)
-  }
-
   
   return (
     <div>
-        <RecipeForm addNewRecipe={addNewRecipe}/>
-        <IngredientsForm addNewIngredient={addNewIngredient}/>
-        {newIngredient.name}
+      <RecipeForm addNewRecipe={addNewRecipe}/>
 
     </div>
   );
--- frontend/src/pages/EditRecipePage/EditRecipePage.js
+++ frontend/src/pages/EditRecipePage/EditRecipePage.js
@@ -1,9 +1,27 @@
+import React, {useState} from "react";
+import useAuth from "../../hooks/useAuth";
+import axios from 'axios';
+
+import IngredientsForm from "../../components/IngredientsForm/IngredientsForm";
+
 
 const EditRecipePage = () => {
-    
+    const [user, token] = useAuth();
+    const [newIngredient, setNewIngredient] = useState([])
+
+    async function addNewIngredient(newIngredient){
+        let response = await axios.post('http://127.0.0.1:8000/api/ingredients/add/', newIngredient, {
+          headers: {
+            Authorization: "Bearer " + token,
+        },
+        })
+        setNewIngredient(response.data)
+      }
+
     return ( 
         <div>
-
+            <IngredientsForm addNewIngredient={addNewIngredient}/>
+            {newIngredient.name}
         </div>
      );
 }
