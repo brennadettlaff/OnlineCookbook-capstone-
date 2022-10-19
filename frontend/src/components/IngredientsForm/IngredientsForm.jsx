b75642509cae7b5a43d34da9c0e27b8d62e80157
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-19 12:17:01 -0600

    added recipe id to ingredients form with useParams and called ingredients component to print ingredients to edit page

--- frontend/src/components/IngredientsForm/IngredientsForm.jsx
+++ frontend/src/components/IngredientsForm/IngredientsForm.jsx
@@ -1,14 +1,18 @@
 import React, {useState} from "react";
+import { useParams } from "react-router-dom";
 
 const IngredientsForm = (props) => {
     const [id] = useState(0);
     const [name, setName] = useState('');
+    let recipe_id = useParams().id
+
 
     function handleSubmit(event){
         event.preventDefault();
         const newIngredient = {
             id: id,
             name: name,
+            recipe_id: recipe_id,
         };
         props.addNewIngredient(newIngredient)
     }
@@ -16,9 +20,8 @@ const IngredientsForm = (props) => {
 
     return ( 
         <div>
-            <h1>Ingredients</h1>
             <form onSubmit={handleSubmit}>
-                <label>Add:</label>
+                <label>Add Ingredient:</label>
                 <input type='text' value={name} onChange={(event) => setName(event.target.value)}></input>
                 <button type='submit'>Add Ingredient</button>
             </form>
--- frontend/src/pages/EditRecipePage/EditRecipePage.js
+++ frontend/src/pages/EditRecipePage/EditRecipePage.js
@@ -1,13 +1,16 @@
 import React, {useState} from "react";
 import useAuth from "../../hooks/useAuth";
 import axios from 'axios';
+import { useParams } from "react-router-dom";
 
 import IngredientsForm from "../../components/IngredientsForm/IngredientsForm";
+import Ingredients from "../../components/Ingredients/Ingredients";
 
 
 const EditRecipePage = () => {
     const [user, token] = useAuth();
     const [newIngredient, setNewIngredient] = useState([])
+    let recipeId = useParams().id
 
     async function addNewIngredient(newIngredient){
         let response = await axios.post('http://127.0.0.1:8000/api/ingredients/add/', newIngredient, {
@@ -20,6 +23,7 @@ const EditRecipePage = () => {
 
     return ( 
         <div>
+            <Ingredients id={recipeId}/>
             <IngredientsForm addNewIngredient={addNewIngredient}/>
             {newIngredient.name}
         </div>
