856eae8770829abf5c99cad11166f122c5259c35
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-20 10:18:21 -0600

    created get all recipe functionality

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,4 +1,19 @@
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
 const SearchPage = () => {
+    const [allRecipes, setAllRecipes] = useState([])
+
+    useEffect(() => {
+        getAllRecipes();
+    })
+
+    async function getAllRecipes(){
+        let response = await axios.get('http://127.0.0.1:8000/api/recipe/')
+        setAllRecipes(response.data)
+        console.log(response.data)
+    }
+
     return ( 
         <div>
             
