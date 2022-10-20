3a8e243c87115bc7691db47ec81692f41f01ea49
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-20 13:14:45 -0600

    added functionality to filter recipes for search term

--- frontend/src/pages/SearchPage/SearchPage.js
+++ frontend/src/pages/SearchPage/SearchPage.js
@@ -1,18 +1,34 @@
 import { useState, useEffect } from "react";
 import axios from 'axios';
+import RecipePage from "../RecipePage/RecipePage";
 
 const SearchPage = (props) => {
     const [allRecipes, setAllRecipes] = useState([])
-    console.log(props.data)
+    const [searchResults, setSearchResults] = useState([])
+
+    let searchTerm = props.data
 
     useEffect(() => {
         getAllRecipes();
-    }, []);
+    }, [searchTerm]);
 
     async function getAllRecipes(){
         let response = await axios.get('http://127.0.0.1:8000/api/recipe/')
         setAllRecipes(response.data)
-        console.log(response.data)
+        if(response.status === 200){
+            filterRecipes(searchTerm);
+        }
+        
+    }
+
+    function filterRecipes(search){
+        let filteredResults = allRecipes.filter((recipe)=>{
+            if(recipe.name.includes(search)){
+                return true
+            }
+        })
+        setSearchResults(filteredResults)
+        console.log(filteredResults)
     }
 
     return ( 
