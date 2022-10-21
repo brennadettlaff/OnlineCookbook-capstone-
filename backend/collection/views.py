da38bc0e84b628f09edccca45760ce351ede7796
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 13:21:49 -0600

    created get functionality in collections views

--- backend/collection/views.py
+++ backend/collection/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+from .models import Collection
+from .serializers import CollectionSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_collections(request):
+    if request.method == 'GET':
+        collection = Collection.objects.filter(user_id=request.user.id)
+        serializer = CollectionSerializer(collection, many=True)
+        return Response(serializer.data)
