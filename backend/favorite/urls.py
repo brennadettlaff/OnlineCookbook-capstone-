c681e3cc0a3b35a1a2a2191da4eeb54d2180c19e
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 08:34:52 -0600

    created get favorites functionality

--- backend/favorite/urls.py
+++ backend/favorite/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.favorite),
+    path('', views.get_favorites),
 
 ]
--- backend/favorite/views.py
+++ backend/favorite/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Favorite
+from .serializers import FavoriteSerializer
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_favorites(request):
+    if request.method == 'GET':
+        recipe = Favorite.objects.filter(user_id=request.user.id)
+        serializer = FavoriteSerializer(recipe, many=True)
+        return Response(serializer.data)
