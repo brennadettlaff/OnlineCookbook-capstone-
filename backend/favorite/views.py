7d997199587f0bd0f56e8d1e34ca88326fc924f6
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 08:53:11 -0600

    add post favorite functionality to views

--- backend/favorite/views.py
+++ backend/favorite/views.py
@@ -6,10 +6,16 @@ from rest_framework.response import Response
 from .models import Favorite
 from .serializers import FavoriteSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def get_favorites(request):
     if request.method == 'GET':
         recipe = Favorite.objects.filter(user_id=request.user.id)
         serializer = FavoriteSerializer(recipe, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = FavoriteSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user = request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
