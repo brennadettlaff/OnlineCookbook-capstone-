32c559333fd130ff34810487075a5d24d827a7a8
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 07:47:27 -0600

    made migrations on favorites app and created serializer

---
+++ backend/favorite/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+
+from .models import Favorite
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ['id', 'user', 'recipe', 'recipe_id']
+        depth = 1
+
+    recipe_id = serializers.IntegerField(write_only=True)
