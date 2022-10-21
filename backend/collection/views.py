facc9ff6b7696f497a1b33bbacfb19ad3a1b2bd4
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 13:29:57 -0600

    created post functionality for collections + made migrations for previous changes to app

--- backend/collection/views.py
+++ backend/collection/views.py
@@ -7,10 +7,16 @@ from django.shortcuts import get_object_or_404
 from .models import Collection
 from .serializers import CollectionSerializer
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 @permission_classes([IsAuthenticated])
 def get_collections(request):
     if request.method == 'GET':
         collection = Collection.objects.filter(user_id=request.user.id)
         serializer = CollectionSerializer(collection, many=True)
         return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = CollectionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user = request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
