972d9edb09ed5a8c5ca7c36664eb65a01ee7eb66
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 09:39:26 -0600

    created add favorite component

---
+++ frontend/src/components/AddFavorite/AddFavorite.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+
+const AddFavorite = () => {
+    return ( 
+        <div>
+
+        </div>
+     );
+}
+ 
+export default AddFavorite;
