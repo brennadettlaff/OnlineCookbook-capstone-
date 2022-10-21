8fcb55db02cb26a6d283100c56ee9170c3b9bc6d
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-21 15:08:15 -0600

    fixed names in recipe_collections model

--- backend/recipe_collection/models.py
+++ backend/recipe_collection/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from recipe.models import Recipe
 from collection.models import Collection
 
-class Favorite(models.Model):
-    user = models.ForeignKey(Collection, on_delete=models.CASCADE)
+class Recipe_Collection(models.Model):
+    Collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
