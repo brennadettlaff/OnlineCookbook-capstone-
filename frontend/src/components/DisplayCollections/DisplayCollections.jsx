ae3679a1e4e3b7327f9406d38ed11dab1f922e17
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-22 12:22:44 -0600

    created display collections component and instantiated in collections page

---
+++ frontend/src/components/DisplayCollections/DisplayCollections.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DispalyCollections = () => {
+    return ( 
+        <div>
+            <>Test</>
+        </div>
+     );
+}
+ 
+export default DispalyCollections;
--- frontend/src/pages/CollectionsPage/CollectionsPage.jsx
+++ frontend/src/pages/CollectionsPage/CollectionsPage.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+import DispalyCollections from "../../components/DisplayCollections/DisplayCollections";
+
 const CollectionsPage = () => {
     return ( 
         <div>
-            
+            <DispalyCollections />
         </div>
      );
 }
