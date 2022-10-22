f779d4f30efb147cd25c69ddd2176f3e4d0d6425
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-22 16:08:41 -0600

    added get collections functionality to add to collection component + created toggle to display collections to be added to

--- frontend/src/components/AddToCollection/AddToCollection.jsx
+++ frontend/src/components/AddToCollection/AddToCollection.jsx
@@ -1,7 +1,47 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+
 const AddToCollection = () => {
+    const [collections, setCollections] = useState([])
+    const [add, setAdd] = useState(true);
+    const [user, token] = useAuth();
+   
+
+    useEffect(() => {
+        getCollections();
+      }, [])
+
+    async function getCollections(){
+        let response = await axios.get('http://127.0.0.1:8000/api/collection/', {
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        })
+        setCollections(response.data)
+    }
+
     return ( 
         <div>
-            Test
+            {add ? (
+                <button onClick={() => setAdd(!add)}>Add Collection</button>
+          ) : (
+            <div>
+                {collections.map((entry, index) => {
+                    return(
+                        <div key={index}>
+                            <div>{entry.name}</div>
+                            <button>Add</button>
+                        </div>
+                    )
+                })}
+
+
+                <button onClick={() => setAdd(!add)}>Cancel</button>
+
+            </div>
+          )}
         </div>
      );
 }
