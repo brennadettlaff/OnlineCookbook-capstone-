75f0bc6987c0e34d783d2721ccb52a2096bcde28
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-22 14:16:53 -0600

    created toggle button in add collection compenent and instantiated on collections page

--- frontend/src/components/AddCollection/AddCollection.jsx
+++ frontend/src/components/AddCollection/AddCollection.jsx
@@ -1,7 +1,14 @@
+import React, { useState } from "react";
+
 const AddCollection = () => {
+    const [add, setAdd] = useState(true);
     return ( 
         <div>
-            
+            {add ? (
+            <button onClick={() => setAdd(!add)}>Add Collection</button>
+          ) : (
+            <button onClick={() => setAdd(!add)}>Cancel</button>
+          )}
         </div>
      );
 }
--- frontend/src/pages/CollectionsPage/CollectionsPage.js
+++ frontend/src/pages/CollectionsPage/CollectionsPage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import AddCollection from "../../components/AddCollection/AddCollection";
 import CollectionsList from "../../components/CollectionsList/CollectionsList";
 
 const CollectionsPage = () => {
     return ( 
         <div>
             <CollectionsList />
+            <AddCollection />
         </div>
      );
 }
