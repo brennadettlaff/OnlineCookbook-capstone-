2f4aad2fbeb4654b091cf0f4bd4468d1d6f4c21a
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-22 12:31:12 -0600

    added functionality to get collections

--- frontend/src/components/DisplayCollections/DisplayCollections.jsx
+++ frontend/src/components/DisplayCollections/DisplayCollections.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
 
 const DispalyCollections = () => {
+    const [collections, setCollections] = useState([])
+    const [user, token] = useAuth();
+
+    useEffect(() => {
+        getCollections();
+      }, [])
+
+    async function getCollections(){
+        let response = await axios.get('http://127.0.0.1:8000/api/collection/', {
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        })
+        setCollections(response.data)
+        console.log(response.data)
+    }
+
     return ( 
         <div>
             <>Test</>
