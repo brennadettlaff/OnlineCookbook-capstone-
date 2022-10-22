ab3a1c1dbdff4e25cdde261d3eab3d1cf3083877
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-22 12:52:51 -0600

    created functionality to get recipe_collections data

--- frontend/src/pages/CollectionPage/CollectionPage.js
+++ frontend/src/pages/CollectionPage/CollectionPage.js
@@ -1,7 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+import { useParams } from "react-router-dom";
+
 const CollectionPage = () => {
+    const [recipes, setRecipes] = useState([])
+    let collectionId = useParams().id
+
+    useEffect(() => {
+        getRecipes(collectionId);
+      }, [])
+
+    async function getRecipes(id){
+        let response = await axios.get(`http://127.0.0.1:8000/api/recipe_collection/collection/${id}/`, {
+        })
+        setRecipes(response.data)
+        console.log(response.data)
+    }
     return ( 
         <div>
-            
+
         </div>
      );
 }
