a127976a01536318e55f710f856855797dbe07bf
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-23 08:45:05 -0600

    set up functionality to edit recipe

--- frontend/src/components/EditRecipe/EditRecipe.jsx
+++ frontend/src/components/EditRecipe/EditRecipe.jsx
@@ -1,4 +1,23 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import useAuth from "../../hooks/useAuth";
+import { useNavigate, useParams } from "react-router-dom";
+
+
 const EditRecipe = () => {
+    const [editRecpie, setEditRecipe] = useState([]);
+    const [user, token] = useAuth();
+    let recipe_id = useParams().id;
+
+    async function editRecipe(newRecipe){
+        let response = await axios.put(`http://127.0.0.1:8000/api/recipe/edit/${recipe_id}/`, newRecipe, {
+            headers: {
+                Authorization: "Bearer " + token,
+            },
+        })
+        setEditRecipe(response.data)
+        }
+
     return ( 
         <div>
             <button>Save</button>
