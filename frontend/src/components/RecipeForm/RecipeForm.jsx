ca8cf3863c89a6954d89dda6836f173129740199
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-10-24 08:17:05 -0600

    updated styling and added postman collection

--- frontend/src/components/RecipeForm/RecipeForm.jsx
+++ frontend/src/components/RecipeForm/RecipeForm.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from "react";
+import './RecipeForm.css'
 
 const RecipeForm = (props) => {
     const [id] = useState(0);
@@ -21,17 +22,29 @@ const RecipeForm = (props) => {
 
 
     return ( 
-        <div>
+        <div className="add-space">
             <h1>New Recipe</h1>
             <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input type='text' value={name} onChange={(event) => setName(event.target.value)}></input>
-                <label>Description:</label>
-                <input type='text' value={description} onChange={(event) => setDescription(event.target.value)}></input>
-                <label>Time:</label>
-                <input type='text' value={time} onChange={(event) => setTime(event.target.value)}></input>
-                <label>Instructions:</label>
-                <textarea type='text' rows='5' value={instructions} onChange={(event) => setInstructions(event.target.value)}></textarea>
+                <div>
+                    <label>Name: </label>
+                    <input type='text' value={name} onChange={(event) => setName(event.target.value)}></input>
+                </div>
+                <div>
+                    <label>Description: </label>
+                    <input type='text' value={description} onChange={(event) => setDescription(event.target.value)}></input>
+                </div>
+                <div>
+                    <label>Time: </label>
+                    <input type='text' value={time} onChange={(event) => setTime(event.target.value)}></input>
+                </div>
+                <div>
+                    <div><label>Instructions:</label> </div>
+                    <textarea type='text' rows='13' cols='80' value={instructions} onChange={(event) => setInstructions(event.target.value)}></textarea>
+                </div>
+                
+                
+                
+                
                 <button type='submit'>Add Recipe</button>
             </form>
         </div>
