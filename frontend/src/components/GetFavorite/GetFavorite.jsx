967e077bb3c9e431a1a2050f71984088959b282a
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-12-15 06:06:12 -0700

    added useParams and argument in get favorite function to get by recipe id

--- frontend/src/components/GetFavorite/GetFavorite.jsx
+++ frontend/src/components/GetFavorite/GetFavorite.jsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import useAuth from "../../hooks/useAuth"; 
+import { useParams } from "react-router-dom";
 
 const GetFavorite = () => {
     const [favorites, setFavorites] = useState([])
     const [user, token] = useAuth();
 
+    let recipe_id = useParams().id
+
     useEffect(() => {
-        getFavorites();
+        getFavorites(recipe_id);
       }, [])
 
-    async function getFavorites(){
-        let response = await axios.get('http://127.0.0.1:8000/api/favorite/recipe/427/', {
+    async function getFavorites(recipe_id){
+        let response = await axios.get(`http://127.0.0.1:8000/api/favorite/recipe/${recipe_id}/`, {
             headers: {
                 Authorization: "Bearer " + token,
             },
