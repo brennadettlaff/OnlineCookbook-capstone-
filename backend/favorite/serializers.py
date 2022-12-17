0ee2faa5bfed0bf57b8b5e1762feb07e962fc10d
Brenna Dettlaff <dettlaffbo@gmail.com>
2022-12-17 14:28:16 -0700

    added boolean field to favorites model and made migrations

--- backend/favorite/serializers.py
+++ backend/favorite/serializers.py
@@ -5,7 +5,7 @@ from .models import Favorite
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
-        fields = ['id', 'user', 'recipe', 'recipe_id']
+        fields = ['id', 'user', 'recipe', 'recipe_id', 'fav_toggle']
         depth = 1
 
     recipe_id = serializers.IntegerField(write_only=True)
