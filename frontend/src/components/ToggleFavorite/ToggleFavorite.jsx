ea6cbcc332a65eee17b67b3177697d2e125a2ce0
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-01-01 21:45:50 -0700

    renamed functionality in ToggleFavorite component

--- frontend/src/components/ToggleFavorite/ToggleFavorite.jsx
+++ frontend/src/components/ToggleFavorite/ToggleFavorite.jsx
@@ -3,38 +3,19 @@ import AddFavorite from "../AddFavorite/AddFavorite"
 import { useState } from "react"
 
 const ToggleFavorite = () => {
-    const [favs, setFavs] = useState([])
-    const [hold, setHold] = useState([])
+    const [favoriteData, setFavoriteData] = useState([])
 
-    function checkFavorites(fav_data){
-        setFavs(fav_data)
-        setHold(fav_data[0])
-
-        console.log("This:", fav_data)
-        console.log(fav_data.length)
-        if(fav_data.length == 0) {
-            console.log("like doesn't exists")
-        }
-        else {
-            console.log('like exists') 
-            console.log(fav_data[0].fav_toggle)
-            if(fav_data[0].fav_toggle == false) {
-                console.log("recipe is not liked")
-            }
-            else {
-                console.log("recipe is liked")
-            }
-        }
-        console.log("Hold", hold)
+    function passedFavorites(fav_data){
+        setFavoriteData(fav_data)
     }
 
     return ( 
         <div>
-            <GetFavorite checkFavorites={checkFavorites}/>
-            {favs.length == 0 &&
+            <GetFavorite checkFavorites={passedFavorites}/>
+            {favoriteData.length == 0 &&
                 <AddFavorite />
             }
-            {favs.length == 1 &&
+            {favoriteData.length == 1 &&
                 <p>Like exists</p>
             }
         </div>
