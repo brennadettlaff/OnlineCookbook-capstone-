e097da45206e99a228dab826e760019126911a1c
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-01-01 20:40:44 -0700

    moved conditional statement to return of ToggleFavorite component

--- frontend/src/components/ToggleFavorite/ToggleFavorite.jsx
+++ frontend/src/components/ToggleFavorite/ToggleFavorite.jsx
@@ -1,8 +1,15 @@
 import GetFavorite from "../GetFavorite/GetFavorite"
+import AddFavorite from "../AddFavorite/AddFavorite"
+import { useState } from "react"
 
 const ToggleFavorite = () => {
+    const [favs, setFavs] = useState([])
+    const [hold, setHold] = useState([])
 
     function checkFavorites(fav_data){
+        setFavs(fav_data)
+        setHold(fav_data[0])
+
         console.log("This:", fav_data)
         console.log(fav_data.length)
         if(fav_data.length == 0) {
@@ -17,11 +24,19 @@ const ToggleFavorite = () => {
             else {
                 console.log("recipe is liked")
             }
-        }}
+        }
+        console.log("Hold", hold)
+    }
 
     return ( 
         <div>
             <GetFavorite checkFavorites={checkFavorites}/>
+            {favs.length == 0 &&
+                <AddFavorite />
+            }
+            {favs.length == 1 &&
+                <p>Like exists</p>
+            }
         </div>
      );
 }
