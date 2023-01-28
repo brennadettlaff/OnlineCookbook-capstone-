f7881d42e61b2d6e8666b646d03231caf6635e84
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-01-28 16:54:08 -0700

    moved display collection recipes functionality to DisplayCollecitonRecipes component

--- frontend/src/components/DisplayCollectionRecipes/DisplayCollectionRecipes.jsx
+++ frontend/src/components/DisplayCollectionRecipes/DisplayCollectionRecipes.jsx
@@ -1,6 +1,34 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
+
 const DisplayCollectionRecipes = () => {
+    const [collection, setCollection] = useState([])
+    let collectionId = useParams().id
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        getCollection(collectionId);
+      }, [])
+
+    async function getCollection(id){
+        let response = await axios.get(`http://127.0.0.1:8000/api/recipe_collection/collection/${id}/`, {
+        })
+        setCollection(response.data)
+    }
+    
     return ( 
-        <div></div>
+        <div>
+            {collection.map((entry, index) => {
+                return(
+                    <div key={index}>
+                        <h3 onClick={() => navigate(`/recipe/${entry.recipe.id}`)}>{entry.recipe.name}</h3>
+                        <div>{entry.recipe.description}</div>
+                    </div>
+                )
+            })}
+        </div>
      );
 }
  
--- frontend/src/pages/CollectionPage/CollectionPage.js
+++ frontend/src/pages/CollectionPage/CollectionPage.js
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import { useParams } from "react-router-dom";
-import { useNavigate } from 'react-router-dom';
 import DisplayCollectionRecipes from "../../components/DisplayCollectionRecipes/DisplayCollectionRecipes";
 
 const CollectionPage = () => {
-    const [collection, setCollection] = useState([])
-    let collectionId = useParams().id
-    let navigate = useNavigate();
-
-    useEffect(() => {
-        getCollection(collectionId);
-      }, [])
-
-    async function getCollection(id){
-        let response = await axios.get(`http://127.0.0.1:8000/api/recipe_collection/collection/${id}/`, {
-        })
-        setCollection(response.data)
-    }
+    
     
     return ( 
         <div>
             <DisplayCollectionRecipes />
-            {collection.map((entry, index) => {
-                return(
-                    <div key={index}>
-                        <h3 onClick={() => navigate(`/recipe/${entry.recipe.id}`)}>{entry.recipe.name}</h3>
-                        <div>{entry.recipe.description}</div>
-                    </div>
-                )
-            })}
+            
         </div>
      );
 }
