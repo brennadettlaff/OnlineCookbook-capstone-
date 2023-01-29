27f40d85f2a2fd251b1b574f467587b2713c3187
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-01-28 17:02:42 -0700

    added get collection detail functionality to CollectionPage

--- frontend/src/pages/CollectionPage/CollectionPage.js
+++ frontend/src/pages/CollectionPage/CollectionPage.js
@@ -1,10 +1,25 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
 import DisplayCollectionRecipes from "../../components/DisplayCollectionRecipes/DisplayCollectionRecipes";
 
 const CollectionPage = () => {
-    
+    const [collectionData, setCollectionData] = useState([])
+    let collectionId = useParams().id
+
+    useEffect(() => {
+        getCollectionDetails(collectionId);
+      }, [])
+
+    async function getCollectionDetails(id){
+        let response = await axios.get(`http://127.0.0.1:8000/api/collection/details/${id}/`, {
+        })
+        setCollectionData(response.data[0])
+    }    
     
     return ( 
         <div>
+            <h2>{collectionData.name}</h2>
             <DisplayCollectionRecipes />
             
         </div>
