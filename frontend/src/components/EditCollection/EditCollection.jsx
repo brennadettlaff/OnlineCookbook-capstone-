29d637166a1eaa489f8f36e970c8607bed5087ec
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-01-30 20:05:14 -0700

    created edit collection form and buttons

--- frontend/src/components/EditCollection/EditCollection.jsx
+++ frontend/src/components/EditCollection/EditCollection.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 const EditCollection = () => {
+    const [toggle, setToggle] = useState(true);
+    const [name, setName] = useState('')
+
+    function handleSubmit(event){
+        event.preventDefault();
+        const newCollection = {
+            name: name,
+        };
+    }
+
     return ( 
         <div>
-            <button>Edit</button>
+            {toggle ? (
+                <div>
+                     <button onClick={()=> setToggle(!toggle)}>Edit</button>
+                </div>
+            ):(
+                <div>
+                    <form onSubmit={handleSubmit}>
+                        <label></label>
+                        <input type='text' value={name} onChange={(event) => setName(event.target.value)}></input>
+                        <button type='submit'>Submit</button>
+                    </form>
+                    <button onClick={() => setToggle(!toggle)}>Save</button>
+                </div>
+            )}
         </div>
      );
 }
