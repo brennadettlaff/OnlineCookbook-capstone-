f7086d3ce1ebb779670a000e6b4644e95a4b8b4b
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-02-12 13:27:50 -0700

    installed bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
