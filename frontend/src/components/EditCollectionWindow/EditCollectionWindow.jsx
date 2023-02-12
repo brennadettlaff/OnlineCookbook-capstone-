ed2639b8cfe1dfc1dcc1ab3a8979a3f09418016e
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-02-12 16:02:45 -0700

    added bootstrap backdrop modal code

--- frontend/src/components/EditCollectionWindow/EditCollectionWindow.jsx
+++ frontend/src/components/EditCollectionWindow/EditCollectionWindow.jsx
@@ -3,11 +3,38 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 function EditCollectionWindow() {
+  const [show, setShow] = useState(false);
 
-    return(
-        <div>
-            Test
-        </div>
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Edit
+      </Button>
+
+      <Modal
+        show={show}
+        onHide={handleClose}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Modal title</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          I will not close if you click outside me. Don't even try to press
+          escape key.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary">Understood</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
