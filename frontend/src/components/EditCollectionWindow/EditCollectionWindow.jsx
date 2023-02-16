50e9673e3736ab4976d2f14025da345202076701
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-02-15 20:06:08 -0700

    renamed EditCollection component to EditCollectionForm for clairity + updated imports

--- frontend/src/components/EditCollectionWindow/EditCollectionWindow.jsx
+++ frontend/src/components/EditCollectionWindow/EditCollectionWindow.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import EditCollection from '../EditCollection/EditCollection';
+import EditCollection from '../EditCollectionForm/EditCollectionForm';
 
 function EditCollectionWindow(props) {
   const [show, setShow] = useState(false);
