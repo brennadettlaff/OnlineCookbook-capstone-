de70207bb8722cf7bb411175f5239098b9b04e10
Brenna Dettlaff <dettlaffbo@gmail.com>
2023-02-19 15:17:07 -0700

    transferred edit collection functionality to EditCollectionWindow

--- frontend/src/components/EditCollectionWindow/EditCollectionWindow.jsx
+++ frontend/src/components/EditCollectionWindow/EditCollectionWindow.jsx
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
-import EditCollection from '../EditCollectionForm/EditCollectionForm';
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+// import EditCollection from '../EditCollectionForm/EditCollectionForm';
 
 function EditCollectionWindow(props) {
   const [show, setShow] = useState(false);
+  const [name, setName] = useState(props.collectionData.name)
+  console.log(name)
+  const [description, setDescription] = useState(props.collectionData.description)
+  const [user, token] = useAuth()
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
-  let collectionInfo = props.collectionData
+  // let collectionInfo = props.collectionData
+  console.log(props.collectionData)
+
+  function handleSubmit(event){
+    event.preventDefault();
+    const newCollectionInfo = {
+        name: name,
+        description: description,
+        user: user,
+    };
+    console.log(newCollectionInfo)
+    editCollection(newCollectionInfo)
+    }
+
+  async function editCollection(newCollectionData){
+    let response = await axios.put(`http://127.0.0.1:8000/api/collection/edit/${props.collectionData.id}/`, newCollectionData, {
+        headers: {
+            Authorization: "Bearer " + token,
+        },
+    })
+    console.log(response)
+    }
   
   return (
     <>
@@ -24,16 +51,31 @@ function EditCollectionWindow(props) {
         keyboard={false}
       >
         <Modal.Header closeButton>
-          <Modal.Title>Edit {collectionInfo.name} Collection</Modal.Title>
+          <Modal.Title>Edit Collection</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <EditCollection collectionInfo={collectionInfo}/>
+          <div>
+            <form>
+              <div>
+                <label>Name</label>
+                <input 
+                  type='text' value={name}  
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </div>
+              <div>
+                <label>Description</label>
+                <input type='text' value={description} onChange={(event) => setDescription(event.target.value)}></input>
+              </div>
+              {/* <button type='submit'>Submit</button> */}
+            </form>
+            </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary">Save</Button>
+          <button variant="primary" type='submit' onClick={handleSubmit}>Save</button>
         </Modal.Footer>
       </Modal>
     </>
